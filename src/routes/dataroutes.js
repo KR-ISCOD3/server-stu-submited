3df4728c1637bdcaf19f3bedc86fa3e0562b7924
KR-ISCOD3 <krisjenxz@example.com>
2024-12-12 10:51:04 +0700

    last-update

--- src/routes/dataroutes.js
+++ src/routes/dataroutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { post_data,delete_data } = require('../controller/dataController.js');
+const { post_data,delete_data, get_all_data } = require('../controller/dataController.js');
 const router = express.Router();
 
+router.get('/',get_all_data);
 router.post('/',post_data);
 router.delete('/:id',delete_data);
 
