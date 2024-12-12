51293b7ed6f3bcfe88244691639d90343ad7cf93
KR-ISCOD3 <krisjenxz@example.com>
2024-12-12 12:15:36 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const { config } = require('dotenv');
-const connectdb = require('./config/connection.js');
-const router = require('./routes/dataroutes.js');
+const express = require("express");
+const cors = require("cors");
+const { config } = require("dotenv");
+const connectdb = require("./config/connection.js");
+const router = require("./routes/dataroutes.js");
 
 config();
 
 const corsOptions = {
-    origin: ['https://form-submit-project.vercel.app/','https://etec-submit-project-form.netlify.app'], // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies and credentials if needed
-  };
-  
+  origin: [
+    "https://form-submit-project.vercel.app",
+    "https://etec-submit-project-form.netlify.app",
+  ], // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // Allow cookies and credentials if needed
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,5 +24,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
